918baa256e001f6e2ed0c0ce85b46bfe0fce9a2b
JuanCastaneda2 <castaneda_juan@javeriana.edu.co>
2021-09-27 16:04:05 -0500

    CI Update

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
- var assert = require('assert')
-
-function test() {
-    assert.equal(2 + 2, 4);
-}
-
-if (module == require.main) require('test').run(test);
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
